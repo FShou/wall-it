1d57b0374aaa4eec00905a36a679036738f6aa97
Faqih Suyudi <faqihsuyudi2020@gmail.com>
2024-12-12 20:30:14 +0800

    commit aja

--- bookmark/build.gradle.kts
+++ bookmark/build.gradle.kts
@@ -42,7 +42,7 @@ dependencies {
     implementation(libs.androidx.coordinatorlayout)
     implementation(libs.material)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.fragment.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
